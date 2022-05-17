e2232bc46b147ac7764a69aa6b7b98431e2c9c47
Cizzy1 <callumandrews30@gmail.com>
2022-05-17 16:28:40 +0100

    Version 3.1

--- Guarani FMP/Assets/Code/Extras/Stats/Currency_system.cs
+++ Guarani FMP/Assets/Code/Extras/Stats/Currency_system.cs
@@ -11,15 +11,8 @@ public class Currency_system : MonoBehaviour
     public float Currency;
     string Key = "Curent";
 
-    void Start(){
-        PlayerPrefs.GetFloat(Key, Currency);
-        //Debug.Log(PlayerPrefs.GetFloat(Key).ToString());
-    }
     void Update()
     {
-        PlayerPrefs.SetFloat(Key, Currency);
-        Amount.text = "₲:" + Currency.ToString();
-        //Debug.Log(Currency.ToString());
-        PlayerPrefs.Save();
+        Amount.text = "₲  " + Currency.ToString();
     }
 }
